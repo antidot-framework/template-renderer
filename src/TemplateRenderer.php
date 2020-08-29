5ec517fa9fcd11226ca2db6a472df3262fc7947e
kpicaza <kpicaza.sound@gmail.com>
2020-08-29 10:17:18 +0200

    fix type issues detected by phpstan

--- src/TemplateRenderer.php
+++ src/TemplateRenderer.php
@@ -6,5 +6,10 @@ namespace Antidot\Render;
 
 interface TemplateRenderer
 {
+    /**
+     * @param string $template
+     * @param array<string, mixed> $data
+     * @return string
+     */
     public function render(string $template, array $data = []): string;
 }
